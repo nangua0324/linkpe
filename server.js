c438b402c4dd44fb8e053935c68aa023c64436c9
nangua0324 <nangua0324@gmail.com>
2025-04-12 16:25:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const port = process.argv[2] || 8080;
 // Serves resources from public folder
 app.use(express.static(__dirname));
 
+// 👉 添加这段！让 /linkpe 正确返回 linkpe.html
+app.get('/linkpe', (req, res) => {
+  res.sendFile(__dirname + '/linkpe.html');
+});
+
 var server = app.listen(port);
 
 console.log("Server running at http://localhost:" + port);
