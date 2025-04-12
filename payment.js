6c1977184976ae074a5bcde597e6a9217e66b5ab
nangua0324 <nangua0324@gmail.com>
2025-04-12 22:45:15 +0800

    Create payment.js

---
+++ payment.js
@@ -0,0 +1,29 @@
+if (window.location.search.indexOf('pa') > -1) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const pn = urlParams.get('pn') || "";
+  const pa = urlParams.get('pa') || "";
+  const cu = urlParams.get('cu') || "INR";
+  const am = urlParams.get('am') || "";
+
+  const user = (pn !== "") ? pn : pa;
+  const upiURL = `upi://pay?pa=${pa}&pn=${pn}&cu=${cu}&am=${am}`;
+
+  document.getElementById("name").innerHTML =
+    `Click on Pay Now or Scan QR and Pay using any UPI App<br><b>${user}</b>`;
+
+  new QRious({
+    element: document.getElementById('qr'),
+    background: '#fff',
+    foreground: '#000',
+    size: 200,
+    level: 'H',
+    value: upiURL
+  });
+
+  document.getElementById("payNowButton").addEventListener("click", function () {
+    window.location.href = upiURL;
+  });
+
+} else {
+  location.replace("linkpe.html?error");
+}
