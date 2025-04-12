0b2e2feb0a4b4c6ed67f03647fec8b1b59646241
nangua0324 <nangua0324@gmail.com>
2025-04-12 16:21:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,11 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+var express = require("express");
+var app = express();
+const port = process.argv[2] || 8080;
 
-// 让浏览器能访问 css/img/qrious 等静态资源
+// Create a server
+// Serves resources from public folder
 app.use(express.static(__dirname));
 
-// 返回 index.html（主页）
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+var server = app.listen(port);
 
-// 返回 linkpe.html（生成 UPI 页面）
-app.get("/linkpe", (req, res) => {
-  res.sendFile(path.join(__dirname, "linkpe.html"));
-});
-
-// API：生成 upi://pay 链接
-app.get("/generate", (req, res) => {
-  const { name, amount, upi } = req.query;
-
-  if (!name || !amount || !upi) {
-    return res.status(400).send("Missing required query parameters");
-  }
-
-  const url = `upi://pay?pa=${upi}&pn=${name}&am=${amount}&cu=INR`;
-  res.send({ upiLink: url });
-});
-
-// 启动服务
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
-});
+console.log("Server running at http://localhost:" + port);
